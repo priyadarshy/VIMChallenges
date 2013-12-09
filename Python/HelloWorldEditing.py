986dfa9deefe2ac50198447dd759fb9aef97f338
ashutosh <ashutosh@eemelabs.com>
2013-12-09 01:18:09 -0800

    Logs keystrokes but doesnt count them

---
+++ Python/HelloWorldEditing.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python
+
+'''
+HelloWorldFinal.py
+
+A first example at code that must be edited to match via VIM.
+
+'''
+
+def a_nice_function(that_takes_args):
+    print 'Hiiii Mom!'
+
+if __name__ == '__main__':
+    a_nice_function(None)
--- Python/start.sh
+++ Python/start.sh
@@ -2,8 +2,12 @@
 
 cp HelloWorldEditingTemplate.py HelloWorldEditing.py
 
-time vi -O HelloWorldEditing.py HelloWorldFinal.py
+time vi -O HelloWorldEditing.py HelloWorldFinal.py -W keystrokes.dat
 wait
 
+echo "Here are the differences that you didn't fix"
 diff HelloWorldEditing.py HelloWorldFinal.py
+
+echo "Here is your keystroke count."
+
 
