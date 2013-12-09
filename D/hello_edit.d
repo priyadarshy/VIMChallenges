742422895e8be9b3256419e07b3eb868b549f97f
Mike Lewis <mikelewis@alphalem.com>
2013-12-09 11:08:15 -0800

    Added The D
    Signed-off-by: Mike Lewis <mikelewis@alphalem.com>

---
+++ D/hello_edit.d
@@ -0,0 +1,11 @@
+#!/usr/bin/env rdmd
+
+import std.stdio;
+
+void hello_world() {
+    writeln("Hello World!");
+}
+
+void main() {
+    hello_world();
+}
---
+++ D/hello_edit_template.d
@@ -0,0 +1,11 @@
+#!/usr/bin/env rdmd
+
+import std.stdio;
+
+void a_nice_function() {
+    writeln("Hiiii Mom!");
+}
+
+void main() {
+    a_nice_function();
+}
